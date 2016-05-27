f4cba28e82f1e45f6166142aac562f709c1a14f4
chuck <chuckinthestar@gmail.com>
2016-05-28 08:20:51 +0900

    find better solution in 002 by python

--- python/02_b.py
+++ python/02_b.py
@@ -1,1 +1,3 @@
-print(('').join(list(filter(lambda c: c not in ['a', 'i', 'u', 'e', 'o'], [i for i in input()]))))
+# print(('').join(list(filter(lambda c: c not in ['a', 'i', 'u', 'e', 'o'], [i for i in input()]))))
+
+print(('').join([i for i in input() if i not in ['a', 'i', 'u', 'e', 'o']]))
